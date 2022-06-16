f91e27c69af55de2bf340fad9131c5a12f9de57c
leonardo <leonardodpnunes@gmail.com>
2022-06-16 01:39:27 -0300

    refactor: wraps each player's movement into moves

--- lib/player.ex
+++ lib/player.ex
@@ -1,5 +1,5 @@
 defmodule ExMon.Player do
-  @required_keys [:name, :attack_one, :attack_two, :heal, :life]
+  @required_keys [:name, :moves, :life]
   @max_life 100
 
   @enforce_keys @required_keys
@@ -9,9 +9,11 @@ defmodule ExMon.Player do
   def build(name, attack_one, attack_two, heal) do
     %ExMon.Player{
       name: name,
-      attack_one: attack_one,
-      attack_two: attack_two,
-      heal: heal,
+      moves: %{
+        attack_one: attack_one,
+        attack_two: attack_two,
+        heal: heal,
+      },
       life: @max_life
     }
   end
