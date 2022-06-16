189085a775376e5ec87fd286f4b183df72b34f3a
leonardo <leonardodpnunes@gmail.com>
2022-06-16 00:40:58 -0300

    feat:  create game agent

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,6 +1,15 @@
 defmodule ExMon do
-  alias ExMon.Player
+  alias ExMon.{Game, Player}
+
+  @computer_name "Thanos"
+
   def create_player(name, attack_one, attack_two, heal) do
     Player.build(name, attack_one, attack_two, heal)
+  end
+
+  def start_game(player) do
+    @computer_name
+    |> create_player("infinite_gauntler_meteor", "snap", "time stone")
+    |> Game.start(player)
   end
 end
