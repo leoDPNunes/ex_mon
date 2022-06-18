4a7ed6910e766b16cf54aa88076d38859984010d
leonardo <leonardodpnunes@gmail.com>
2022-06-18 03:04:53 -0300

    feat: print the attack message

--- lib/game/status.ex
+++ lib/game/status.ex
@@ -6,8 +6,16 @@ defmodule ExMon.Game.Status do
     IO.inspect(Game.info())
     IO.puts("-------------------------------------")
   end
-  
+
   def print_wrong_move_message(move) do
     IO.puts("\n ===== Invalid move: #{move}! =====\n")
+  end
+
+  def print_move_message(:computer, :attack, damage) do
+    IO.puts("\n ===== The Player attacked the Computer dealing #{damage} of damage! =====\n")
+  end
+
+  def print_move_message(:player, :attack, damage) do
+    IO.puts("\n ===== The Computer attacked the Player dealing #{damage} of damage! =====\n")
   end
 end
